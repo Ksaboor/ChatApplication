9a0b8eb00ebc3d39c15fd55036ddbac79e6d51a6
Khail Saboor <khalilsaboor1@gmail.com>
2018-11-22 03:10:52 -0500

    added client && static files

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var usernamePage = document.querySelector('#username-page');
+var chatPage = document.querySelector('#chat-page');
+var usernameForm = document.querySelector('#usernameForm');
+var messageForm = document.querySelector('#messageForm');
+var messageInput = document.querySelector('#message');
+var messageArea = document.querySelector('#messageArea');
+var connectingElement = document.querySelector('#.connecting');
+
+var stompClient = null;
+var username = null;
+
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+function connect(event) {
+    username = document.querySelector('#name').value.trim();
+
+
+}
+
+function onConnection() {
+
+}
+
+function onError(error) {
+
+}
+
+function sendMessage(event) {
+
+}
+
+function onMessageReceived(payload) {
+
+}
+
+function getAvatarColor(messageSender) {
+
+}
+
+usernameForm.addEventListener('submit', connect, true)
+usernamePage.addEventListener('submit', sendMessage, true)
